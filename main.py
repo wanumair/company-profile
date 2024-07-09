9d54b5bd06721b15dad1414b12bcc40b28bbefa4
wanumair <wanumair1209@gmail.com>
2024-07-09 10:47:00 +0800

    add images

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ col1, col2, col3 = st.columns([1, 1, 1])
 with col1:
     for index, row in df[:4].iterrows():
         st.header(row['first name'].capitalize() + " " + row['last name'].capitalize())
-
+        st.write(row['role'])
+        st.image("images/" + row['image'])
 
 
